c513fc10b44bbb24b939713d6b4a7b05d500cf43
Arnav1373 <80662620+Arnav1373@users.noreply.github.com>
2021-07-28 17:23:30 +0530

    Linux Manual - Presented by Arnav Srivastava
    This function helps to know about various command used in Linux Terminal

---
+++ manual.c
@@ -0,0 +1,45 @@
+// This function help us to know the various command used in Linux Terminal
+void manual()
+{
+    printf("\n");
+    printf("\n");
+    printf("\t\t\t\033[4m\033[93m Welcome To Techies Terminal Module \033[0m\t\n");
+    printf("\n");
+    printf("\n");
+    printf("\033[93mgedit \033[0m>>> a text editor for the Desktop\n");
+    printf("\n");
+    printf("\033[93mcd \033[0m>>> To change directory\n");
+    printf("\n");
+    printf("\033[93mls \033[0m>>> To see the content of the directory\n");
+    printf("\n");
+    printf("\033[93mrm \033[0m>>> To delete the directory\n");
+    printf("\n");
+    printf("\033[93mwget \033[0m>>> To download any fie from the internet\n");
+    printf("\n");
+    printf("\033[93mhistory \033[0m>>> To review the previous command used\n");
+    printf("\n");
+    printf("\033[93mmkdir \033[0m>>> To create a directory or folder\n");
+    printf("\n");
+    printf("\033[93menv \033[0m>>> To put your environment variables insides a file\n");
+    printf("\n");
+    printf("\033[93mifconfig enp0s3 \033[0m>>> To know the current I.P address\n");  
+    printf("\n");
+    printf("\033[93mgrep \033[0m>>> It will search a word in the long file easily\n");
+    printf("\n");
+    printf("\033[93mclear \033[0m>>> To clear the screen\n");
+    printf("\n"); 
+    printf("\033[93mdate \033[0m>>> To show date and time\n");
+    printf("\n"); 
+    printf("\033[93mmv \033[0m>>> To move one file from one place to another \n");
+    printf("\n"); 
+    printf("\033[93mpwd \033[0m>>> To see the directory(location) and files\n");
+    printf("\n"); 
+    printf("\033[93mcp \033[0m>>> To copy one file to new file or append to a existing file\n");
+    printf("\n"); 
+    printf("\033[93mecho \033[0m>>> To display the given text on the terminal window\n");
+    printf("\n"); 
+    printf("\033[93msleep \033[0m>>> To delay the execution\n");
+    printf("\n");
+    printf("\033[93mexit \033[0m>>> To exit\n");
+    printf("\n");
+}
