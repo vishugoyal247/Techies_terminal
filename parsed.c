79e42f7218b8a3053b776d95b922ae4626fa8916
tulsirajput <87925254+tulsirajput@users.noreply.github.com>
2021-07-28 19:01:19 +0530

    Add files via upload
    Parsing command words- Presented by Tulsi

---
+++ parsed.c
@@ -0,0 +1,21 @@
+
+// function for parsing command words
+
+void parseSpace(char* str, char** parsed)// parsed is the address of string 
+{
+    int i;
+    /*strsep() is a predefined function in the library string.h. It seperates the string 
+	into pieces marked by the given delimiter. It returns NULL when string is empty*/
+   
+    for (i = 0; i < MAXLIST; i++)      
+	{
+        parsed[i] = strsep(&str, " "); // strsep is used here to seperate the string based on space and stores string in respective command address
+  
+        if (parsed[i] == NULL)         // if the command is empty, we'll come out of the loop
+            break;
+            
+        if (strlen(parsed[i]) == 0)    // if length of command is zero, then value of i will decrease and iteration will stop 
+            i--;
+    }
+}
+
