edda979dad9705f04c5a17c3b3d2f77fabbb8ed7
TanishkGupta19 <85075310+TanishkGupta19@users.noreply.github.com>
2021-07-29 15:46:06 +0530

    processing function -- presented by Tanishk Gupta
    processString : It will process the string when the cmd came.
    Piped : A pipe is a system call that creates a unidirectional communication link between two file descriptors.

---
+++ Processing_Function.c
@@ -0,0 +1,22 @@
+int processString(char* str, char** parsed, char** parsedpipe)
+{
+
+	char* strpiped[2];
+	int piped = 0;
+
+	piped = parsePipe(str, strpiped);
+
+	if (piped) {
+		parseSpace(strpiped[0], parsed);
+		parseSpace(strpiped[1], parsedpipe);
+
+	} else {
+
+		parseSpace(str, parsed);
+	}
+
+	if (ownCmdHandler(parsed))
+		return 0;
+	else
+		return 1 + piped;
+}
